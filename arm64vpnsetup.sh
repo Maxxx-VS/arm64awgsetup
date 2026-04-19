f0c4b3bd1ccc9201fc371d3440a2930f3fe0e07f
Maksim Vysochanskii <hot-zinc@yandex.ru>
2026-04-19 10:28:58 +0600

    Update arm64vpnsetup.sh

--- arm64vpnsetup.sh
+++ arm64vpnsetup.sh
@@ -17,7 +17,7 @@ git clone https://github.com/amnezia-vpn/amneziawg-go /tmp/amneziawg-go
 cd /tmp/amneziawg-go
 go build -v -o amneziawg-go
 # Переносим бинарник в системную папку
-sudo cp amnezia-go /usr/local/bin/
+sudo cp amneziawg-go /usr/local/bin/
 
 # Сборка awg-quick
 git clone https://github.com/amnezia-vpn/amneziawg-tools.git /tmp/amnezia-tools
@@ -28,6 +28,6 @@ sudo make install
 # Убираем сбивающие dns настройки домашних сетей
 sudo sed -i '/nameserver 192.168.0.1/d' /etc/resolv.conf
 sudo sed -i '/nameserver 192.168.1.1/d' /etc/resolv.conf
-sudo sed -i '/nameserver 10.3.101.1/d' /etc/resolv.conf
+sudo sed -i '/nameserver 10.10.4.1/d' /etc/resolv.conf
 
 echo "А теперь нужно добавить свой конфигурационный файл в /etc/amnezia/amneziawg/<ИМЯ>.conf\nЗапускать командой: 'sudo WG_QUICK_USERSPACE_IMPLEMENTATION=amnezia-go awg-quick up <ИМЯ>'"
